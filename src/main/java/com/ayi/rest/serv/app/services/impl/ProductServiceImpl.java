b1c13c9be06e885c8551226b574dbe6c1fbf2fe6
NClemente91 <nicoclemente91@gmail.com>
2022-10-19 10:56:56 -0300

    C16 - Modificacion en service de producto

--- src/main/java/com/ayi/rest/serv/app/services/impl/ProductServiceImpl.java
+++ src/main/java/com/ayi/rest/serv/app/services/impl/ProductServiceImpl.java
@@ -97,8 +97,6 @@ public class ProductServiceImpl implements IProductService {
 
         Category categoryFilter = categoryMapper.responseDtoToEntity(categoryService.findCategoryByName(category));
 
-        System.out.println(categoryFilter.getProductsList());
-
         Pageable pageable = PageRequest.of(page, size);
 
         Page<Product> productPageList = productRepository.findAllByCategory(categoryFilter, pageable);
